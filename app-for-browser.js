217cf45c21e11752417d2b1582d9c9c5dd033f40
Tarchcom <139373603+Tarchcom@users.noreply.github.com>
2023-07-12 20:44:45 +0200

    my first js code

---
+++ app-for-browser.js
@@ -0,0 +1,9 @@
+let answer1, answer2, answer3;
+answer1 = prompt("Чи подобається програмування на JAvaScript?", "yes/no");
+answer2 = prompt("Чи знаєте ви, що таке змінні та як їх використовувати?", "yes/no");
+answer3 = prompt("Чи бажаєте ви більше дізнатися про JavaScript?", "yes/no");
+
+console.log("Ваші відповіді:");
+console.log("1. " +answer1);
+console.log("2. " +answer2);
+console.log("3. " +answer3);
